fe5af78b75232ce33a96ff8f84a549f979e2f05e
slenderballa <gswe4645@740s-pl-132.local>
2015-12-17 08:28:48 -0700

    Initial Commit

---
+++ Silly Project 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Silly Project 2
+//
+//  Created by Swenson, Garrett on 12/17/15.
+//  Copyright © 2015 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
