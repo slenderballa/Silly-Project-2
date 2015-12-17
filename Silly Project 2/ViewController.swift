687e2d302bb54c0c5c8cf45fd6e3a93520ebc5c0
slenderballa <gswe4645@740s-pl-132.local>
2015-12-17 09:44:55 -0700

    Finished product

--- Silly Project 2/ViewController.swift
+++ Silly Project 2/ViewController.swift
@@ -8,17 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+
+class ViewController: UIViewController
+{
+    
+    
+    var newRed: CGFloat = 0
+    var newGreen: CGFloat = 0
+    var newBlue: CGFloat = 0
+
+    
+    
+    
+    func changeColor()
+    {
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
+
+
+  
+    @IBAction func RedSlider(sender: UISlider)
+    {
+        newRed = CGFloat(Double(sender.value))
+        changeColor()
+    }
+    
+    
+    
+    @IBAction func GreenSlider(sender: UISlider)
+    {
+        
+       newGreen = CGFloat(Double(sender.value))
+        changeColor()
+        
+    }
+    
+    
+    @IBAction func BlueSlider(sender: UISlider)
+    {
+        
+       newBlue = CGFloat(Double(sender.value))
+        changeColor()
+        
+    }
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    
+    
+    
+    
+    
+    
 
 
 }
